cea9a300effd71b50bf0d5a64b4113aa25321499
Marc Ypes <c33ram@gmail.com>
2018-06-11 03:55:58 +0200

    add modal

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
+let clickingFav = false;
 $( "i" ).click(function() {
+    clickingFav = true;
     $( this ).toggleClass( "far" ).toggleClass( "fas" );
     let act = ($( this ).data("arg1") );
     let isFavorite = localStorage.getItem(act);
@@ -7,6 +9,19 @@ $( "i" ).click(function() {
     } else {
         localStorage.setItem(act, 'favorited')
     }
+});
+
+$("td").click(function() {
+    if (clickingFav) {
+        clickingFav = false;
+        return;
+    }
+    let url = ($(this).data("href") );
+    if (url) {
+        $('#myModal').modal('show');
+        $("#show").attr("src", url);
+    }
+
 });
 
 $( document ).ready(function() {
