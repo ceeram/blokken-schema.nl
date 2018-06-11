5a5dfe248c706cf7cbc0727bb4ac240c2ed6e5aa
Marc Ypes <c33ram@gmail.com>
2018-06-11 09:36:47 +0200

    Add popups

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 let clickingFav = false;
-$( "i" ).click(function() {
+$( 'i' ).click(function() {
     clickingFav = true;
-    $( this ).toggleClass( "far" ).toggleClass( "fas" );
-    let act = ($( this ).data("arg1") );
+    $( this ).toggleClass( 'far' ).toggleClass( 'fas' );
+    let act = ($( this ).data('arg1') );
     let isFavorite = localStorage.getItem(act);
     if (isFavorite) {
         localStorage.removeItem(act);
@@ -11,25 +11,23 @@ $( "i" ).click(function() {
     }
 });
 
-$("td").click(function() {
-    if (clickingFav) {
-        clickingFav = false;
-        return;
-    }
-    let url = ($(this).data("href") );
-    if (url) {
-        $('#myModal').modal('show');
-        $("#show").attr("src", url);
-    }
-
-});
-
-$( document ).ready(function() {
+$(document).ready(function() {
     let values = [], keys = Object.keys(localStorage), i = keys.length;
     while ( i-- ) {
         let favorited = localStorage.getItem( keys[i] );
         if (favorited === 'favorited') {
-            $(this).find(`[data-arg1='${keys[i]}']`).toggleClass( "far" ).toggleClass( "fas" );
+            $(this).find(`[data-arg1='${keys[i]}']`).toggleClass( 'far' ).toggleClass( 'fas' );
         }
     }
+});
+$('#myModal').on('show.bs.modal', function (event) {
+    if (clickingFav) {
+        clickingFav = false;
+        return false;
+    }
+    let url = $(event.relatedTarget).data('href');
+    if (!url) {
+        return false;
+    }
+    $('#show').attr('src', url);
 });
