5af2851e44f39e8d269f7fe6e29959dca5c18ac7
Marc Ypes <c33ram@gmail.com>
2018-06-12 17:58:46 +0200

    Add bio for zach and phil

--- assets/script.js
+++ assets/script.js
@@ -48,9 +48,19 @@ $('#special-act-modal').on('show.bs.modal', function (event) {
         return false;
     }
     let td = $(event.relatedTarget); // Button that triggered the modal
+    let videoUrl = td.data('video-url');
     let modal = $(this);
+    let video = modal.find('video');
+    if (videoUrl) {
+        video.html('<source src="' + videoUrl + '" type="video/mp4">');
+        video.show();
+    } else {
+        video.html('');
+        video.hide();
+    }
+
     modal.find('#special-act-modal-label').text(td.data('title'));
     modal.find('#special-act-time').text(td.data('time'));
     modal.find('#special-act-location').text(td.data('location'));
-    modal.find('.modal-body').html(td.data('description'));
+    modal.find('.modal-body').find('.description').html(td.data('description'));
 });
